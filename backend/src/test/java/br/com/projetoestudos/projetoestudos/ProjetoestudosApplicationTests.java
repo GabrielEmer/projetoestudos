572123b096667583954634f2af21cbbd1c6b8134
ellianm <ellianmarcondes@hotmail.com>
2020-11-23 22:54:49 -0300

    tes

---
+++ backend/src/test/java/br/com/projetoestudos/projetoestudos/ProjetoestudosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetoestudos.projetoestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoestudosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

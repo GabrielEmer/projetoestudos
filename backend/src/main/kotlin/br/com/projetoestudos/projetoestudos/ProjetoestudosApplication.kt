2f4e7ee6cb1f7e0acd4202cef24c00c9d2c58449
ellianm <ellianmarcondes@hotmail.com>
2020-11-23 22:14:07 -0300

    backend

---
+++ backend/src/main/kotlin/br/com/projetoestudos/projetoestudos/ProjetoestudosApplication.kt
@@ -0,0 +1,11 @@
+package br.com.projetoestudos.projetoestudos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetoestudosApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjetoestudosApplication>(*args)
+}

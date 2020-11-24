f8824002e2c1b9e84ee593d2ee6077479f0196f9
ellianm <ellianmarcondes@hotmail.com>
2020-11-23 23:29:18 -0300

    aaa

--- backend/src/main/java/br/com/projetoestudos/projetoestudos/model/User.java
+++ backend/src/main/java/br/com/projetoestudos/projetoestudos/model/User.java
@@ -1,9 +1,9 @@
 package br.com.projetoestudos.projetoestudos.model;
 
-        import lombok.Getter;
-        import lombok.Setter;
+import lombok.Getter;
+import lombok.Setter;
 
-        import javax.persistence.*;
+import javax.persistence.*;
 
 @Entity(name = "user")
 @Getter

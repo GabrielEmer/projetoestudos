94135ece653a6f31262230f6e62f81e70be1dc2f
ellianm <ellianmarcondes@hotmail.com>
2020-11-25 13:22:20 -0300

    hello world exemplo

---
+++ frontend/src/helloWorld/helloWorld.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './helloworld.css';
+
+class HelloWorld extends Component {
+
+    render() {
+        return <h1 className="helloworld">Hello World</h1>
+    }
+
+}
+
+export default HelloWorld;

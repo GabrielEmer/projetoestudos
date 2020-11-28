6d5aaf0bc0c24680ef7bb245615ae8cbcec34499
ellianm <ellianmarcondes@hotmail.com>
2020-11-28 17:14:25 -0300

    menuzão

--- frontend/src/menu-bar/menu-bar.js
+++ frontend/src/menu-bar/menu-bar.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 
-class MenuBar extends Component {
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
+
+class MenuBar extends Component {
     render() {
-        return (<AppBar position="static">
-            <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                    News
-        </Typography>
-                <Button color="inherit">Login</Button>
-            </Toolbar>
-        </AppBar>)
+        return (
+            <div className={useStyles.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" className={useStyles.menuButton} color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" className={useStyles.title}>
+                            News
+                  </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
     }
 
 }
